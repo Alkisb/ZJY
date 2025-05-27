71aa28f22892e9e2056245df23b70d89dfafa972
Alekisb <5401466+zjy1998888@user.noreply.gitee.com>
2025-05-28 05:10:13 +0800

    多个hello world

--- day1.java
+++ day1.java
@@ -1,5 +1,12 @@
 public class day1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
