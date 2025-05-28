ebd36feace7c945ee5ab77f5458cdecd835634b4
Alekisb <5401466+zjy1998888@user.noreply.gitee.com>
2025-05-28 20:42:42 +0800

    dev-zjy

--- day1.java
+++ day1.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class day1 {
     public static void main(String[] args) {
+         int a = 1+1;
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
@@ -10,5 +15,11 @@ public class day1 {
         System.out.println("asuydfyuasdfuasyfdas");
         System.out.println("secend");
         System.out.println("hello world");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        List<Integer> collect = list.stream().map(x -> x + 1).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
